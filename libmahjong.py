d80b4caa7533e73f4343bc42a02a8885bcf1391b
sasag.ks@gmail.com <sasag.ks@gmail.com>
2022-05-05 03:58:34 +0900

    make game

--- libmahjong.py
+++ libmahjong.py
@@ -6,33 +6,82 @@ import time
 import glob
 import random
 
+# =======================
+# BGM
+# =======================
+# musicフォルダ配下数字(シーン別)フォルダに放り込んだファイルがランダムに再生される
+# 以下シーン一覧
+# 0: タイトル画面
+
+# =======================
+# SE
+# =======================
+# 数字で指定
+# 0: mouseover.ogg  ボタンのマウスオーバー時
+# 1: click.ogg  何もないところをクリックした時
+# 2: enter.ogg  決定音
+
+
 # システム
 bgm_files = []
+playsound = []
+
+def exitgame():
+    if pygame.mixer.music.get_busy():
+        pygame.mixer.music.fadeout(100)
+    sys.exit()
+
+def playbgm(n):
+    if playsound[0]:
+        bgm_idx = random.randrange(0, len(bgm_files[n]))
+        bgm_file = bgm_files[n][bgm_idx]
+        if pygame.mixer.music.get_busy():
+            pygame.mixer.music.fadeout(100)
+            time.sleep(0.1)
+        pygame.mixer.music.load(bgm_file)
+        pygame.mixer.music.play(-1)
+
+def playse(name):
+    if playsound[1]:
+        pass
+
+def change_bgm_mode():
+    if playsound[0]:
+        playsound[0] = False
+        #if pygame.mixer.music.get_busy():
+        pygame.mixer.music.pause()
+    else:
+        playsound[0] = True
+        pygame.mixer.music.unpause()
+
+    # config.iniを書き換える
+
+def change_se_mode():
+    playsound[1] = not playsound[1]
+    # config.iniを書き換える
+
+def get_bgm_stats():
+    return playsound[0]
+
+def get_se_stats():
+    return playsound[1]
 
 # ゲーム起動時に1回だけ必ず実行される
 def init():
     for i in range(1):
         path = "./music/" + str(i) + "/*"
         bgm_files.append(glob.glob(path))
-
-def exitgame():
-    if pygame.mixer.music.get_busy():
-        pygame.mixer.music.fadeout(100)
-    sys.exit()
     
-def playbgm(n):
-    bgm_idx = random.randrange(0, len(bgm_files[n]))
-    bgm_file = bgm_files[n][bgm_idx]
-    if pygame.mixer.music.get_busy():
-        pygame.mixer.music.fadeout(100)
-        time.sleep(0.1)
-    pygame.mixer.music.load(bgm_file)
-    pygame.mixer.music.play(-1)
+    # config.iniから読み込むように変更する
+    playsound.append(True)
+    playsound.append(True)
+
 
 # 麻雀
 
+
 # イーシャンテンなら2，聴牌なら1，アガリなら0を返す．それ以外は-1
-def check_hands(tiles, naki, riichi, dora):
+def check_hands(tiles, riichi, dora):
     # Dots      : 1~9 : 筒子
     # Bamboo    : 11~19 : 索子
     # Characters: 21~29 : 萬子
--- window.py
+++ window.py
@@ -1,12 +1,39 @@
 import pygame
 from pygame.locals import *
-import sys
+#import sys
+import random
 
 import libmahjong
 
-# 定数,変数の宣言 
+# 定数の宣言 
 SCREEN_SIZE = (800, 600)  # 画面サイズ
 TITLE = "Nightfall Mahjong"
+
+# "sysimg/"+str(0)+"/"+TILES[n]+".gif"
+TILES = ["p_no_", "p_ms1_", "p_ms2_", "p_ms3_", "p_ms4_", "p_ms5_", "p_ms6_", "p_ms7_", "p_ms8_", "p_ms9_",
+         "p_ji_h_", "p_ps1_", "p_ps2_", "p_ps3_", "p_ps4_", "p_ps5_", "p_ps6_", "p_ps7_", "p_ps8_", "p_ps9_",
+         "p_ji_c_", "p_ss1_", "p_ss2_", "p_ss3_", "p_ss4_", "p_ss5_", "p_ss6_", "p_ss7_", "p_ss8_", "p_ss9_",
+         "p_ji_e_", "p_ji_s_", "p_ji_w_", "p_ji_n_"]
+
+
+def nanno_koma(n): # testok
+    s = ""
+    if n >= 30:
+        namelist = ["東", "南", "西", "北"]
+        s += namelist[n - 30]
+    elif n % 10 == 0:
+        namelist = ["白", "發", "中"]
+        s += namelist[int(n / 10)]
+    elif n >= 20:
+        namelist = ["一", "二", "三", "四", "五", "六", "七", "八" ,"九"]
+        s += namelist[n % 10 - 1] + "萬"
+    else:
+        if n >= 10:
+            s += "索子(ソーズ)の"
+        else:
+            s += "筒子(ピンズ)の"
+        s += str(n%10)
+    return s
 
 # 初期化
 pygame.init()
@@ -14,27 +41,22 @@ screen = pygame.display.set_mode(SCREEN_SIZE)
 pygame.display.set_caption(TITLE)
 font = pygame.font.Font(None, 55)
 
-def title():
+def title(refreshbgm=True):
     # BGM開始
-    libmahjong.playbgm(0)
-
-    # 文字
-    title_text = font.render("NIGHTFALL MAHJONG", True, (222, 222, 222))
-    menu_text = font.render("GAME START", True, (222, 222, 222))
+    if refreshbgm:
+        libmahjong.playbgm(0)
 
     # ボタン
-    menu_button   = pygame.Rect(250, 360, 300, 60)
-    option_button = pygame.Rect(250, 450, 300, 60)
+    start_button   = pygame.Rect(153, 330, 196, 211)
+    config_button = pygame.Rect(455, 330, 196, 211)
+    startimg = pygame.image.load("sysimg/button/start.png")
+    configimg = pygame.image.load("sysimg/button/config.png")
 
-    #while True:
-    screen.fill((20,20,20))
-
-    pygame.draw.rect(screen, (0, 80, 0), menu_button)
-    pygame.draw.rect(screen, (0, 80, 0), option_button)
-
-    screen.blit(title_text, [100, 100])
-    screen.blit(menu_text, [275, 375])
-
+    # 背景画像
+    bgimg = pygame.image.load("bgimg/title.png")
+    screen.blit(bgimg, (0, 0))
+    screen.blit(startimg, (153, 337))
+    screen.blit(configimg, (455, 337))
 
     pygame.display.update()
 
@@ -45,30 +67,63 @@ def title():
                 libmahjong.exitgame()
             
             if event.type == MOUSEBUTTONDOWN:
-                if menu_button.collidepoint(event.pos):
+                if start_button.collidepoint(event.pos):
                     return 2
-                elif option_button.collidepoint(event.pos):
+                elif config_button.collidepoint(event.pos):
                     return 1
 
 def option():
     # フルスクリーンに切り替えるかどうか
     # 音量の設定
+    # 背景画像
+    bgimg = pygame.image.load("bgimg/config.png")
+    
+    backtotitle1 = pygame.Rect(0, 0, 100, 600)
+    backtotitle2 = pygame.Rect(700, 0, 100, 600)
+
+    # ボタン
+    bgm_toggle   = pygame.Rect(380, 240, 271, 76)
+    se_toggle = pygame.Rect(380, 340, 271, 76)
+
+    toggleonimg = pygame.image.load("sysimg/button/on.png")
+    toggleoffimg = pygame.image.load("sysimg/button/off.png")
+
+    bgmstats = True
+    sestats = True
+
+    def update():
+        # 描画
+        screen.blit(bgimg, (0, 0))
+        bgmstats = libmahjong.get_bgm_stats()
+        if bgmstats:
+            screen.blit(toggleonimg, (380, 240))
+        else:
+            screen.blit(toggleoffimg, (380, 240))
+        
+        sestats = libmahjong.get_se_stats()
+        if sestats:
+            screen.blit(toggleonimg, (380, 340))
+        else:
+            screen.blit(toggleoffimg, (380, 340))
+        pygame.display.update()
+    
+    update()
 
     while True:
-        screen.fill((20,20,20))
-
-        pygame.draw.rect(screen, (0, 80, 0), Rect(10, 10, 80, 50))
-        text = font.render("OPTION", True, (222, 222, 222))
-        screen.blit(text, [100, 100])
-
-        pygame.display.update()
         # イベント処理
         for event in pygame.event.get():
             if event.type == QUIT:  # 終了イベント
                 libmahjong.exitgame()
             
             if event.type == MOUSEBUTTONDOWN:
-                return 0
+                if bgm_toggle.collidepoint(event.pos):
+                    libmahjong.change_bgm_mode()
+                    update()
+                elif se_toggle.collidepoint(event.pos):
+                    libmahjong.change_se_mode()
+                    update()
+                elif backtotitle1.collidepoint(event.pos) or backtotitle2.collidepoint(event.pos):
+                    return 10
 
 def menu():
     # 四人，三人の選択
@@ -89,7 +144,7 @@ def menu():
         pygame.draw.rect(screen, (0, 80, 0), san_button)
 
         screen.blit(yon_text, [275, 375])
-        screen.blit(san_text, [275, 400])
+        screen.blit(san_text, [275, 550])
 
         pygame.display.update()
         # イベント処理
@@ -102,10 +157,10 @@ def menu():
                     game()
                     return 0
                 elif san_button.collidepoint(event.pos):
-                    game(players=3)
+                    game(playernum=3)
                     return 0
 
-def game(players=4, sibari=0):
+def game(playernum=4, sibari=0):
 
     # Dots      : 1~9 : 筒子
     # Bamboo    : 11~19 : 索子
@@ -113,23 +168,88 @@ def game(players=4, sibari=0):
     # Dragons   : 0, 10, 20 : 白，ハツ，中
     # Winds     : 30, 31, 32, 33 : 東，南，西，北
 
+    img_playertiles = []
+
     # 毎回ソートして，表示用の配列を別に作る
 
     # 初期化
-    player1 = {"tiles":[], "wind":0}
-    player2 = {"tiles":[], "wind":0}
-    player3 = {"tiles":[], "wind":0}
-    player4 = {"tiles":[], "wind":0}
-    wall = [i for i in range(34)]
+    players = [{"tiles":[], "wind":0}, # 0, 自分
+               {"tiles":[], "wind":0}, # 1, 右どなり(下家)
+               {"tiles":[], "wind":0}, # 2, 対面
+               {"tiles":[], "wind":0}] # 3, 左どなり(上家)
+    wall = [i for i in range(34) for j in range(4)]
+    #for i in range(34):
+    #    for j in range(4):
+    #        wall.append(i)
+    prevailing_wind = 30 # 東
 
-    # 親決め
+
+    # 親決め testok
+    def choose_dealer(): # sitting_a_seat
+        # →4人の中から一人がサイコロを振る
+        kariton = random.randrange(0, playernum)
+        #print("仮東は", kariton)
+        
+        # ↑の位置にサイコロを表示
+        # サイコロを二個振る
+        dice1 = random.randint(1, 6)
+        dice2 = random.randint(1, 6)
+        #print("サイコロの出目は ", dice1, dice2)
+        karioya = (kariton + dice1 + dice2) % playernum
+        #print("仮親は ", karioya)
+
+        # サイコロを二個振る
+        dice1 = random.randint(1, 6)
+        dice2 = random.randint(1, 6)
+        #print("サイコロの出目は ", dice1, dice2)
+        oya = (karioya + dice1 + dice2) % playernum
+        #print("親は ", oya)
+
+        for i in range(playernum):
+            players[(oya + i) % playernum]["wind"] = 30 + i
+            #print((oya+i)%playernum, 30+i)
+    
+    choose_dealer()
+        
+    
     # シャッフル，配牌
-    # ドラ決め?
+    random.shuffle(wall)
+    # いつかちゃんとした配り方も実装したい，今はとりあえず完全ランダム
+    for i in range(4):
+        for j in range(13):
+            players[i]["tiles"].append(wall.pop())
 
-    #
+    # ドラ決め testok
+    bonus_tile = wall.pop()
+    #print("ドラ表示牌は", bonus_tile)
+    if bonus_tile >= 30:
+        bonus_tile = 30 + (bonus_tile % 30 + 1) % 4
+    elif bonus_tile % 10 == 0:
+        bonus_tile = (bonus_tile + 10) % 30
+    elif bonus_tile % 10 == 9:
+        bonus_tile -= 8
+    else:
+        bonus_tile += 1
+    print("ドラは", nanno_koma(bonus_tile))
+
+    scale = 1.4
+    size_x = int(33 * scale)
+    size_y = int(60 * scale)
+
+    x = 20
+    y = 600 - size_y
 
     #開始時アニメーション
     screen.fill((20,20,20))
+
+    players[0]["tiles"] = sorted(players[0]["tiles"])
+
+    for t in players[0]["tiles"]:
+        timgpath = "sysimg/"+str(0)+"/resize/"+TILES[t]+str(0)+".jpg"
+        timg = pygame.image.load(timgpath)
+        #timg = pygame.transform.rotozoom(timg, 0, scale) # 拡大，プレビュー用
+        screen.blit(timg, (x, y))
+        x += size_x
 
     text = font.render("GAME", True, (222, 222, 222))
     screen.blit(text, [100, 100])
@@ -137,8 +257,6 @@ def game(players=4, sibari=0):
     pygame.display.update()
     
     while True:
-
-        screen.fill((20,20,20))
 
         # 
 
@@ -153,6 +271,7 @@ def game(players=4, sibari=0):
                 return
 
 
+# main
 stats = 0 # 0:title, 1:option, 2:menu, 3:custommenu, 4:game, 5:result
 settings_game = []
 settings_option = []
@@ -162,6 +281,8 @@ while True:
     print(stats)
     if stats == 0:
         stats = title()
+    elif stats == 10:
+        stats = title(refreshbgm=False)
     elif stats == 1:
         stats = option()
     elif stats == 2:
